288796955bc5ab30e91c791f09b672e23f044fc5
Lucas <lucasdonuts@gmail.com>
2021-01-05 18:22:07 -0600

    Created navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Add Rack::LiveReload // doesn't work
+  # config.middleware.insert_before Rack::Lock, Rack::LiveReload
+
+  # Devise default url
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
